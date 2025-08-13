09239fac63c09d89aa4cda4fde622ba76fff270f
Gabriel <gabriel.avila.ga09@gmail.com>
2025-08-13 18:24:07 -0500

    Added SecurityConfig to access h2 console

---
+++ lifequest/src/main/java/com/gabetech/lifequest/config/SecurityConfig.java
@@ -0,0 +1,26 @@
+package com.gabetech.lifequest.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests((requests) ->
+              requests.requestMatchers("/h2-console/**").permitAll()
+                    .anyRequest().authenticated());
+        http.headers(headers -> headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin));
+        http.csrf(CsrfConfigurer::disable);
+        http.formLogin(withDefaults());
+        http.httpBasic(withDefaults());
+        return http.build();
+    }
+}
